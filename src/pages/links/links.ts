a0d832c919092febf7566d65619dffba4af509b5
Jimmy Mau <jimmy.b.mau@gmail.com>
2024-11-09 23:48:33 -0500

    Add initial links and tags

---
+++ src/pages/links/links.ts
@@ -0,0 +1,16 @@
+const links = [
+    {
+        name: "Jonny Kim on the Jocko Willink podcast",
+        date: null,
+        url: "https://www.youtube.com/watch?v=yujP3-AxXsI",
+        tags: ["podcast", "self-help", "video"]
+    },
+    {
+        name: "Don't share your goals",
+        date: null,
+        url: "https://www.youtube.com/watch?v=NHopJHSlVo4",
+        tags: ["self-help", "video"]
+    },
+]
+
+export default links
