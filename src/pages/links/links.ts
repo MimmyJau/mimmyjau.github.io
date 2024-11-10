af15028370913b287c40291ee12d74aef29a9b5f
Jimmy Mau <jimmy.b.mau@gmail.com>
2024-11-10 00:09:43 -0500

    Add sorting links by date

--- src/pages/links/links.ts
+++ src/pages/links/links.ts
@@ -1,13 +1,13 @@
 const links = [
     {
         name: "Jonny Kim on the Jocko Willink podcast",
-        date: null,
+        dateAdded: "2022-01-07",
         url: "https://www.youtube.com/watch?v=yujP3-AxXsI",
         tags: ["podcast", "self-help", "video"]
     },
     {
         name: "Don't share your goals",
-        date: null,
+        dateAdded: "2021-05-27",
         url: "https://www.youtube.com/watch?v=NHopJHSlVo4",
         tags: ["self-help", "video"]
     },
